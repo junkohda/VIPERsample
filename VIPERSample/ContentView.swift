3816d9905272014eab220020e8bbbbe31ad2d267
jun.kohda <jun.koda@mazurica.com>
2024-06-05 15:10:29 +0900

    VIPERのテンプレート

---
+++ VIPERSample/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  VIPERSample
+//
+//  Created by jun.kohda on 2024/06/05.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ VIPERSample/VIPERSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  VIPERSampleApp.swift
+//  VIPERSample
+//
+//  Created by jun.kohda on 2024/06/05.
+//
+
+import SwiftUI
+
+@main
+struct VIPERSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
